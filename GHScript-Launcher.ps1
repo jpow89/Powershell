c4c6bba2564276d77fa1b4ca5628c76ede684a3a
jpow89 <139915122+jpow89@users.noreply.github.com>
2023-12-15 11:44:29 -0600

    Update GHScript-Launcher.ps1

--- GHScript-Launcher.ps1
+++ GHScript-Launcher.ps1
@@ -1,17 +1,14 @@
 $repoUrl = "https://api.github.com/repos/jpow89/Powershell/contents/"
-$webRequest = [System.Net.WebRequest]::Create($repoUrl)
-$webRequest.Headers.Add("User-Agent", "PowerShell Script")
-$response = $webRequest.GetResponse()
-$reader = New-Object System.IO.StreamReader($response.GetResponseStream())
-$json = $reader.ReadToEnd() | ConvertFrom-Json
-$response.Close()
+$headers = @{
+    "User-Agent" = "PowerShell Script"
+}
 
-$scripts = $json | Where-Object { $_.name -like "*.ps1" }
-$selectedScript = $scripts | Select-Object -ExpandProperty name | Out-GridView -Title "Select a script to run" -OutputMode Single
+$scripts = Invoke-RestMethod -Uri $repoUrl -Headers $headers
+$scriptNames = $scripts | Where-Object { $_.name -like "*.ps1" } | Select-Object -ExpandProperty name
+$selectedScript = $scriptNames | Out-GridView -Title "Select a script to run" -OutputMode Single
 
 if ($selectedScript) {
     $scriptUrl = $scripts | Where-Object { $_.name -eq $selectedScript } | Select-Object -ExpandProperty download_url
     $scriptContent = (Invoke-WebRequest -Uri $scriptUrl).Content
     Invoke-Expression $scriptContent
 }
-
