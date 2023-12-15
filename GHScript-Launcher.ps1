948f5c3aede0e8dd6676bc7defc4b2e7da7b8bb5
jpow89 <139915122+jpow89@users.noreply.github.com>
2023-12-15 11:46:13 -0600

    Update GHScript-Launcher.ps1

--- GHScript-Launcher.ps1
+++ GHScript-Launcher.ps1
@@ -1,9 +1,14 @@
 $repoUrl = "https://api.github.com/repos/jpow89/Powershell/contents/"
-$headers = @{
-    "User-Agent" = "PowerShell Script"
-}
 
-$scripts = Invoke-RestMethod -Uri $repoUrl -Headers $headers
+# Use a WebClient to set the User-Agent header
+$webClient = New-Object System.Net.WebClient
+$webClient.Headers.Add("User-Agent", "PowerShell Script")
+
+# Download the JSON response and convert it to an object
+$jsonResponse = $webClient.DownloadString($repoUrl)
+$scripts = $jsonResponse | ConvertFrom-Json
+
+# Filter and select the script
 $scriptNames = $scripts | Where-Object { $_.name -like "*.ps1" } | Select-Object -ExpandProperty name
 $selectedScript = $scriptNames | Out-GridView -Title "Select a script to run" -OutputMode Single
 
